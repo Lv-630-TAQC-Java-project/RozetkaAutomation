f9ecdd16d053d5f71cff6fca7811c16ee0751a05
percifalll <genero.frido@gmail.com>
2021-10-24 01:08:20 +0300

    .gitignore update. Now filtering .exe files

--- src/main/java/com/ss/ita/google/ui/pages/Header.java
+++
@@ -1,9 +0,0 @@
-package com.ss.ita.google.ui.pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class Header extends BasePage{
-    public Header(WebDriver driver) {
-        super(driver);
-    }
-}
