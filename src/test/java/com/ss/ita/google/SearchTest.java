2b98fd68e21f4a94ee8f09c28768bb01f6a0bb38
vladdmytriv <vladdmutriv@gmail.com>
2021-10-25 14:06:01 +0300

    some changes

--- src/test/java/com/ss/ita/google/SearchTest.java
+++ src/test/java/com/ss/ita/google/SearchTest.java
@@ -3,6 +3,7 @@ package com.ss.ita.google;
 import com.ss.ita.google.ui.pages.HomePage;
 import com.ss.ita.google.ui.pages.SearchResultPage;
 import com.ss.ita.runner.TestRunner;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,14 +14,26 @@ import static org.testng.Assert.*;
 public class SearchTest extends TestRunner {
 
     @Test
-    public void verifySearchTest() {
+    public void verifyChangeColorTest() {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String searchText = "funny kitten picture";
 
-        String firstLinkText=new HomePage(driver)
-                .search("funny kitten")
+        SearchResultPage searchResultPage = new HomePage(driver)
+                .search(searchText);
+        String firstLinkText = searchResultPage
                 .getLinkText(0);
 
-        String expected = "99% Lose this TRY NOT TO LAUGH Challenge";
-        assertEquals(firstLinkText, expected);
+//        assertTrue(firstLinkText.contains(searchText));
+
+        String defaultColor = searchResultPage
+                .getLinksColor();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //  js.executeScript("document.querySelector('//div[@id='rso']//h3[contains(@class,'LC20lb')][1]').innerText=" +"pink");
+        js.executeScript("document.querySelector('#rso > div:nth-child(26) > div > div:nth-child(1) > div > div > div > div.yuRUbf > a > h3').style.color = 'lightblue';");
+        String changedColor = searchResultPage.getLinksColor();
+
+        assertTrue(defaultColor != changedColor);
+
     }
 }
