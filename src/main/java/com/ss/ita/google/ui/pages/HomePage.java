e5d6f235fe1078e894397e847bf8259b17091c09
NazarYakh <nazaryakh@gmail.com>
2021-10-28 09:44:24 +0300

    some fix

--- src/main/java/com/ss/ita/google/ui/pages/HomePage.java
+++ src/main/java/com/ss/ita/google/ui/pages/HomePage.java
@@ -1,13 +1,9 @@
 package com.ss.ita.google.ui.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HomePage extends BasePage {
-    public HomePage(WebDriver driver) {
-        super(driver);
-    }
+public class HomePage {
 
     public SearchResultPage doSearch(String searchText) {
         return setSearchTerms(searchText).search();
