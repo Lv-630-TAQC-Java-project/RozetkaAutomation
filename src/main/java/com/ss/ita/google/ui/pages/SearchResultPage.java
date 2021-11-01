5e3cdbddc399f6e6064a17f8fa1eb2d56c95e1a0
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-01 17:26:36 +0200

    remove goToSearchResultPage(int numberSearchResultPage)

--- src/main/java/com/ss/ita/google/ui/pages/SearchResultPage.java
+++ src/main/java/com/ss/ita/google/ui/pages/SearchResultPage.java
@@ -50,34 +50,8 @@ public class SearchResultPage {
         driver.findElement(By.xpath("//a/img")).click();
         return this;
     }
-    
-	public SearchResultPage goToSearchResultPage(int numberSearchResultPage) {
-		List<WebElement> searchResultPages = driver.findElements(By.xpath("//td")).subList(1,
-				driver.findElements(By.xpath("//td")).size() - 1);
-		int indexLastPage = Integer.parseInt(searchResultPages.get(searchResultPages.size() - 1).getText()) - 1;
-		if (numberSearchResultPage - 1 <= indexLastPage) {
-			searchResultPages.get(numberSearchResultPage - 1).click();
-		}
-		if (numberSearchResultPage - 1 > indexLastPage) {
-			while (true) {
-				String PrevLastPageText = searchResultPages.get(searchResultPages.size() - 1).getText();
-				searchResultPages.get(indexLastPage).click();
-				searchResultPages = driver.findElements(By.xpath("//td")).subList(1,
-						driver.findElements(By.xpath("//td")).size() - 1);
-				if (numberSearchResultPage - 1 <= indexLastPage) {
-					searchResultPages.get(numberSearchResultPage - 1).click();
-					break;
-				}
-				if (PrevLastPageText.equals(searchResultPages.get(searchResultPages.size() - 1).getText())) {
-					break;
-				}
-			}
-		}
-		return new SearchResultPage();
-	}
-	
-	public WebDriver goToResultLink (int numberSearchResultPage, int numberResultLink) {
-		new SearchResultPage().goToSearchResultPage(numberSearchResultPage);
+    	
+	public WebDriver goToResultLink (int numberResultLink) {
 		List<WebElement> searchResultLink = driver.findElements(By.xpath("//div[@class='yuRUbf']/a"));
 		System.out.println(searchResultLink.size());
 		searchResultLink.get(numberResultLink - 1).click();
