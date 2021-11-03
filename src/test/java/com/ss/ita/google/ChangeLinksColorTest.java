f7a0fc722e587508dc04aa5f6ad0afd7eddf0f57
vladdmytriv <vladdmutriv@gmail.com>
2021-11-03 10:07:43 +0200

    fix for comments

--- src/test/java/com/ss/ita/google/ChangeLinksColorTest.java
+++ src/test/java/com/ss/ita/google/ChangeLinksColorTest.java
@@ -2,24 +2,27 @@ package com.ss.ita.google;
 
 import com.ss.ita.google.ui.pages.*;
 import com.ss.ita.google.ui.pages.runnerAndProperties.TestRunner;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static com.ss.ita.google.ui.pages.util.WebElementUtil.*;
 import static org.testng.Assert.*;
 
 public class ChangeLinksColorTest extends TestRunner {
-
+    // method to get link needed in test
+    private WebElement searchedResultLink(SearchResultPage searchResultPage) {
+        return searchResultPage.getSearchedResultLink(0);
+    }
+    
     @Test
     public void verifyChangingLinksColor() {
-        int numberOfSearchedResultLink = 0; //first link to get text and color.
-
         SearchResultPage searchResultPage = new HomePage()
                 .doSearch("Funny kitten picture");
         String firstLinkText = searchResultPage
-                .getSearchedResultLinkText(numberOfSearchedResultLink);
-        String defaultColor = getColor(searchResultPage.getSearchedResultLink(numberOfSearchedResultLink));
-        setColorViaJs(searchResultPage.getSearchedResultLink(numberOfSearchedResultLink), "lightpink");
-        String changedColor = getColor(searchResultPage.getSearchedResultLink(numberOfSearchedResultLink));
+                .getSearchedResultLinkText(0);
+        String defaultColor = getColor(searchedResultLink(searchResultPage));
+        setColorViaJs(searchedResultLink(searchResultPage), "lightpink");
+        String changedColor = getColor(searchedResultLink(searchResultPage));
 
         assertTrue(firstLinkText.contains("Kitten"));
         assertTrue(defaultColor != changedColor);
