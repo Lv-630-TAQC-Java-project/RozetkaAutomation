3d6372f10a6e2518cc54083e47b0af2d57332b98
dzhyv <d.zhyvko@gmail.com>
2021-11-05 09:55:44 +0200

    Migrated methods to Selenide

--- src/main/java/com/ss/ita/google/ui/pages/SearchResultPage.java
+++ src/main/java/com/ss/ita/google/ui/pages/SearchResultPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.ss.ita.google.ui.pages.runnerAndProperties.TestRunner.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchResultPage {
 
@@ -57,11 +57,11 @@ public class SearchResultPage {
     }
 
     public SearchResultPage openResultPage(int pageNumber) {
-        driver.findElement(By.xpath(String.format("//a[@aria-label = 'Page %s']", pageNumber))).click();
+        $x(String.format("//a[@aria-label = 'Page %s']", pageNumber)).click();
         return this;
     }
 
     public int getCurrentPageNumber() {
-        return Integer.parseInt(driver.findElement(By.xpath("//td[text()]")).getText());
+        return Integer.parseInt($x("//td[text()]").getText());
     }
 }
