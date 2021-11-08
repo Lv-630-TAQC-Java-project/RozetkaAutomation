54d8160c5be7bd6c4ae0384c5d34c0f28ca775d7
dzhyv <d.zhyvko@gmail.com>
2021-11-08 09:29:30 +0200

    Revert "Change assert to selenide should method"
    This reverts commit 330e655fb4319ee1ae9158e86683218aaceaa7c0.

--- src/main/java/com/ss/ita/google/ui/pages/SearchResultPage.java
+++ src/main/java/com/ss/ita/google/ui/pages/SearchResultPage.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Condition.*;
 
 public class SearchResultPage {
 
@@ -62,8 +61,7 @@ public class SearchResultPage {
         return this;
     }
 
-    public SearchResultPage shouldHavePageNumber(int number) {
-        $x("//td[text()]").shouldHave(text(Integer.toString(number)));
-        return this;
+    public int getCurrentPageNumber() {
+        return Integer.parseInt($x("//td[text()]").getText());
     }
 }
