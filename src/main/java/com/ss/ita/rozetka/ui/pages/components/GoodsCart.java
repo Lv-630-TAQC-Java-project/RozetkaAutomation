163f9c1d96aaccf2eab77289c28bebbe2d35f514
percifalll <genero.frido@gmail.com>
2021-11-09 11:43:03 +0200

    Created page object for goods cart

---
+++ src/main/java/com/ss/ita/rozetka/ui/pages/components/GoodsCart.java
@@ -0,0 +1,34 @@
+package com.ss.ita.rozetka.ui.pages.components;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+
+import java.util.NoSuchElementException;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class GoodsCart {
+    // This method should open order page and return it.
+    // It is not currently because of order page absence.
+    @Deprecated
+    public Object order() {
+        return null;
+    }
+
+    public String getSumWithCurrency() {
+        return $x("//div[contains(@class,'sum-price')]").getAttribute("textContent");
+    }
+
+    public int getSum() {
+        return Integer.parseInt(getSumWithCurrency().replaceAll("\\D", ""));
+    }
+
+    public boolean isEmpty() {
+        return getGoodsList().isEmpty();
+    }
+
+    private ElementsCollection getGoodsList() {
+        return $$x("//li[contains(@class, 'cart-list__item')]");
+    }
+}
