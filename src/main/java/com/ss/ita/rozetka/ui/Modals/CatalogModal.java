7029a4fc8386c5c76e5650ddda47f85d5a3f01b6
anastasia8755 <anastasia.sydor331@gmail.com>
2021-11-11 00:04:47 +0200

    First version

--- src/main/java/com/ss/ita/rozetka/ui/Modals/CatalogModal.java
+++ src/main/java/com/ss/ita/rozetka/ui/Modals/CatalogModal.java
@@ -2,6 +2,12 @@ package com.ss.ita.rozetka.ui.Modals;
 
 import com.ss.ita.rozetka.ui.pages.*;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class CatalogModal extends Header {
-    //for writing methods in close future
+
+    public boolean isCatalogModalDisplayed(){
+    return $x("/html/body/app-root/div/div/rz-header/header/div/div/rz-header-fat-menu/fat-menu/div/ul/li[6]/div").isDisplayed();
+    //html/body/app-root/div/div/rz-header/header/div/div/rz-header-fat-menu/fat-menu/div/ul/li[6]/div
+    }
 }
---
+++ src/test/com/ss/ita/rozetka/OpeningAngClosingCatalogTest.java
@@ -0,0 +1,21 @@
+package com.ss.ita.rozetka;
+
+import com.ss.ita.rozetka.ui.Modals.CatalogModal;
+import com.ss.ita.rozetka.ui.pages.HomePage;
+import com.ss.ita.rozetka.ui.runner.TestRunner;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class OpeningAngClosingCatalogTest extends TestRunner {
+
+    @Test
+    public void verifyThatCatalogOpensAndCloses(){
+        CatalogModal catalogModal = new HomePage().open().openCatalogModal();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(catalogModal.isCatalogModalDisplayed());
+        catalogModal.openCatalogModal();
+        softAssert.assertFalse(catalogModal.isCatalogModalDisplayed());
+        softAssert.assertAll();
+
+    }
+}
