e1aca0f1c0a9c29148d9c374a24543faf52ad89f
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-10 16:56:48 +0200

    fix closeHamburgerMethod

--- src/main/java/com/ss/ita/rozetka/ui/Modals/HamburgerModal.java
+++ src/main/java/com/ss/ita/rozetka/ui/Modals/HamburgerModal.java
@@ -1,5 +1,6 @@
 package com.ss.ita.rozetka.ui.Modals;
 
+import com.codeborne.selenide.Condition;
 import com.ss.ita.rozetka.ui.pages.*;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -8,6 +9,7 @@ public class HamburgerModal {
 
     public Header closeHamburgerModal() {
         $x("//button[@class='side-menu__close']").click();
+        $x("//nav[@class='drawer ng-star-inserted']").shouldNotBe(Condition.visible);
         return new Header();
     }
 }
