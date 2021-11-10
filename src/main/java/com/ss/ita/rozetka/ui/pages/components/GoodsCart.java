2681f903048c14775f34ee0ea0187132bb4b8542
percifalll <genero.frido@gmail.com>
2021-11-10 13:58:11 +0200

    Refactored method. Added methods for products - edit count, remove.

--- src/main/java/com/ss/ita/rozetka/ui/pages/components/GoodsCart.java
+++ src/main/java/com/ss/ita/rozetka/ui/pages/components/GoodsCart.java
@@ -1,10 +1,14 @@
 package com.ss.ita.rozetka.ui.pages.components;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import com.ss.ita.rozetka.ui.pages.OrderingPage;
 
-import static com.codeborne.selenide.Selenide.$$x;
-import static com.codeborne.selenide.Selenide.$x;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class GoodsCart {
     public OrderingPage order() {
@@ -12,19 +16,51 @@ public class GoodsCart {
         return new OrderingPage();
     }
 
-    public String getSumWithCurrency() {
-        return $x("//div[contains(@class,'sum-price')]").getAttribute("textContent");
-    }
-
-    public int getSum() {
-        return Integer.parseInt(getSumWithCurrency().replaceAll("\\D", ""));
+    public int getProductsTotalPrice() {
+        String sum = $x("//div[contains(@class,'sum-price')]").getAttribute("textContent");
+        return Integer.parseInt(sum.replaceAll("\\D", ""));
     }
 
     public boolean isEmpty() {
+        $("single-modal-window").should(Condition.exist);
         return getGoodsList().isEmpty();
     }
 
     private ElementsCollection getGoodsList() {
         return $$x("//li[contains(@class, 'cart-list__item')]");
+    }
+
+    private SelenideElement getProductByName(String productName) {
+        String productXpath = String.format(
+                "//single-modal-window//li[contains(., '%s')]",
+                productName);
+        return $x(productXpath);
+    }
+
+    public List<String> getProductNames(){
+        return getGoodsList().stream()
+                .map(product -> product.$x(".//a[@class='cart-product__title']").text())
+                .collect(Collectors.toList());
+    }
+
+    public GoodsCart setProductCountByName(String productName, int count) {
+        SelenideElement countField = getProductByName(productName).$x(".//input[contains(@class, 'cart-counter__input')]");
+        countField.clear();
+        countField.sendKeys(count + "");
+        waitForCirclingDonutToDisappear();
+        return this;
+    }
+
+    private void waitForCirclingDonutToDisappear() {
+        String loadingDonutXpath = "//div[contains(@class,'preloader_with_donut')]";
+        $x(loadingDonutXpath).should(Condition.exist);
+        $x(loadingDonutXpath).shouldNot(Condition.exist);
+    }
+
+    public GoodsCart removeProductByName(String productName) {
+        SelenideElement product = getProductByName(productName);
+        product.$x(".//button[contains(@id, 'cartProductActions')]").click();
+        product.$x(".//rz-trash-icon/button").click();
+        return this;
     }
 }
