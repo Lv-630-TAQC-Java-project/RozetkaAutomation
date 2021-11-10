0a39472c41fe40d898250bc2ea71fab6e67cf8c5
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-10 16:22:12 +0200

    fix open method

--- src/main/java/com/ss/ita/rozetka/ui/pages/HomePage.java
+++ src/main/java/com/ss/ita/rozetka/ui/pages/HomePage.java
@@ -1,5 +1,7 @@
 package com.ss.ita.rozetka.ui.pages;
 
+import com.codeborne.selenide.Selenide;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 import static java.lang.String.format;
@@ -9,7 +11,7 @@ public class HomePage extends Header {
     private static final String GENERAL_PRODUCT_CATEGORY_TEMPLATE = "//div[contains(@class,'menu-wrapper_state_static')]/descendant::a[contains(@href,'%s')]";
 
     public HomePage open() {
-        open("https://rozetka.com.ua");
+        Selenide.open("https://rozetka.com.ua");
         return this;
     }
 
