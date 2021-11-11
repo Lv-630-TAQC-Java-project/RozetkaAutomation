25adc956b4a819c71bfe0cb8a0e8f4b16de85cd0
dzhyv <d.zhyvko@gmail.com>
2021-11-11 09:30:06 +0200

    Rename method

--- src/test/java/com/ss/ita/rozetka/clickRozetkaLogoTest.java
+++ src/test/java/com/ss/ita/rozetka/clickRozetkaLogoTest.java
@@ -1,17 +1,15 @@
-package com.ss.ita.rozetka.rozetka;
+package com.ss.ita.rozetka;
 
 import com.ss.ita.rozetka.ui.pages.HomePage;
-import com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory;
 import org.testng.Assert;
 import  org.testng.annotations.Test;
 
 import static com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory.PRODUCTS_FOR_HOUSE;
-import static com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory.GARDEN_TECH_CATEGORY;
 
 public class clickRozetkaLogoTest {
 
     @Test
-    public void verifyClickRozetkaLogo(){
+    public void verifyClickOnRozetkaLogoRedirectsToHomePageLogo(){
         String actualUrl = new HomePage().open()
                 .openProductCategoryPage(PRODUCTS_FOR_HOUSE.getName())
                 .openHomePage()
