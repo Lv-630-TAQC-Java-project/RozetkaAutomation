1b83c655709b24eaa71957fd96d75dc838413601
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-11 10:51:02 +0200

    fix SearchHistoryTest

--- src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
@@ -1,6 +1,7 @@
 package com.ss.ita.rozetka.ui;
 
 import com.ss.ita.rozetka.ui.pages.HomePage;
+import com.ss.ita.rozetka.ui.elements.Header;
 import com.ss.ita.rozetka.ui.runner.TestRunner;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,8 +12,8 @@ public class SearchHistoryTest extends TestRunner {
     public void verifySearchHistoryText() {
         int numberSearchTerm = 1;
         String searchTerm = "DELL";
-        String actualTerm = new HomePage().open().doSearch(searchTerm).openHomePage().setFocusSearchInput()
-                .getTextFromSearchHistory(numberSearchTerm);
+        String actualTerm = new HomePage().open().header.doSearch(searchTerm).header.openHomePage()
+                .header.setFocusSearchInput().getTextFromSearchHistory(numberSearchTerm);
         Assert.assertEquals(actualTerm, searchTerm);
     }
 }
