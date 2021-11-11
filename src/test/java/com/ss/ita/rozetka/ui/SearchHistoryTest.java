0aeba152681f8f31f08aea5d5865ab9c8aa8d334
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-11 09:38:35 +0200

    SearchHistoryTest

---
+++ src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
@@ -0,0 +1,18 @@
+package com.ss.ita.rozetka.ui;
+
+import com.ss.ita.rozetka.ui.pages.HomePage;
+import com.ss.ita.rozetka.ui.runner.TestRunner;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchHistoryTest extends TestRunner {
+
+    @Test
+    public void verifySearchHistoryText() {
+        int numberSearchTerm = 1;
+        String searchTerm = "DELL";
+        String actualTerm = new HomePage().open().doSearch(searchTerm).openHomePage().setFocusSearchInput()
+                .getTextFromSearchHistory(numberSearchTerm);
+        Assert.assertEquals(actualTerm, searchTerm);
+    }
+}
