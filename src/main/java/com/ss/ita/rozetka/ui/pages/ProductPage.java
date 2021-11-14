9909966388904b67ddb714b1e61940d3cf57cf13
percifalll <genero.frido@gmail.com>
2021-11-14 21:45:39 +0200

    Merged with main

--- src/main/java/com/ss/ita/rozetka/ui/pages/ProductPage.java
+++ src/main/java/com/ss/ita/rozetka/ui/pages/ProductPage.java
@@ -1,20 +1,13 @@
 package com.ss.ita.rozetka.ui.pages;
 
 import com.ss.ita.rozetka.ui.Modals.BasketModal;
-import com.ss.ita.rozetka.ui.elements.Header;
 
 import static com.codeborne.selenide.Selenide.$x;
 
-public class ProductPage extends Header {
-
-    public ProductPage addProductToComparison() {
-        $x("//button[@class='compare-button ng-star-inserted']").click();
-        return this;
-    }
+public class ProductPage extends HeaderPage {
 
     public BasketModal addProductToBasket() {
         $x("//button[contains(@class,'buy-button button button_')]").click();
         return new BasketModal();
     }
-
 }
