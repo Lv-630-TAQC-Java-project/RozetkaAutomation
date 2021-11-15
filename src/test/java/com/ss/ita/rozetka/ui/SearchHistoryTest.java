bc701d87a0e4e275b1be823ae3e9b9070f9b32d5
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-15 16:44:18 +0200

    fit SearchHistoryTest

--- src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
@@ -21,16 +21,18 @@ public class SearchHistoryTest extends TestRunner {
                             .getHeader()
                             .isUrlContainsText(partOfUrl),
                 "Search Result didn't open");
-        HomePage homePage =  productsList.getHeader().openHomePage();
+        HomePage homePage =  productsList
+                                .getHeader()
+                                .openHomePage();
         Assert.assertEquals(homePage
                                 .getHeader()
                                 .getCurrentUrl(),
                     "https://rozetka.com.ua/",
                     "Home Page didn't open");
         String actualSearchTerm = homePage
-                                .getHeader()
-                                .setFocusSearchInput()
-                                .getTextFromSearchHistory(numberSearchTerm);
+                                    .getHeader()
+                                    .setFocusSearchInput()
+                                    .getTextFromSearchHistory(numberSearchTerm);
         Assert.assertEquals(actualSearchTerm, searchTerm);
    }
 }
