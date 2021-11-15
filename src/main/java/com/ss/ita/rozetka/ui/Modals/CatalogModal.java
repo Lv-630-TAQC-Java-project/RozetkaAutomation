ec576d954bc0f1958fb355cebc91fc0d10898871
anastasia8755 <anastasia.sydor331@gmail.com>
2021-11-15 09:52:35 +0200

    fix newline

--- src/main/java/com/ss/ita/rozetka/ui/Modals/CatalogModal.java
+++ src/main/java/com/ss/ita/rozetka/ui/Modals/CatalogModal.java
@@ -2,7 +2,6 @@ package com.ss.ita.rozetka.ui.Modals;
 
 import com.ss.ita.rozetka.ui.pages.HeaderPage;
 
-
 public class CatalogModal extends HeaderPage {
     //for writing methods in close future
 }
