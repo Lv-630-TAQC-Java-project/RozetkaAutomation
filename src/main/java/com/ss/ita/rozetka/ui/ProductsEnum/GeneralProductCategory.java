6c95de2f21eb2088a1c0497202dd52d84755d048
Yura <yurkakuryan@gmail.com>
2021-11-15 09:09:52 +0200

    fixes

--- src/main/java/com/ss/ita/rozetka/ui/ProductsEnum/GeneralProductCategory.java
+++ src/main/java/com/ss/ita/rozetka/ui/ProductsEnum/GeneralProductCategory.java
@@ -5,7 +5,7 @@ public enum GeneralProductCategory {
     NOTEBOOKS_AND_COMPUTERS("computers-notebooks"),
     PRODUCTS_FOR_HOUSE("bt"),
     COTTAGE_GARDEN_BACKYARD("dacha-sad-ogorod");
-    
+
     private final String name;
 
     GeneralProductCategory(String name) {
