5eb3c9fb98b13ab2ed8c103b2f32de29643f0b79
percifalll <genero.frido@gmail.com>
2021-11-15 20:44:38 +0200

    Created additional methods

--- src/main/java/com/ss/ita/rozetka/ui/pages/ProductPage.java
+++ src/main/java/com/ss/ita/rozetka/ui/pages/ProductPage.java
@@ -2,12 +2,26 @@ package com.ss.ita.rozetka.ui.pages;
 
 import com.ss.ita.rozetka.ui.Modals.BasketModal;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ProductPage extends HeaderPage {
-
     public BasketModal addProductToBasket() {
         $x("//button[contains(@class,'buy-button button button_')]").click();
         return new BasketModal();
+    }
+
+    public ProductPage addToComparison() {
+        $x("//button[contains(@class, 'compare-button')]").click();
+        return this;
+    }
+
+    public int getPrice() {
+        String value = $x("//p[contains(@class, 'product-prices__big')]").text();
+        return Integer.parseInt(value.replaceAll("\\D", ""));
+    }
+
+    public String getName() {
+        return $("h1").text();
     }
 }
