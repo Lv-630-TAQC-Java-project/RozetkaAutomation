f3e667e48e34c501e7bb2bb02ae230d14ab05c16
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-16 15:33:03 +0200

    fix test? fix code format

--- src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/SearchHistoryTest.java
@@ -1,8 +1,10 @@
 package com.ss.ita.rozetka.ui;
 
 import com.ss.ita.rozetka.ui.TestUtils.TestRunner;
+import com.ss.ita.rozetka.ui.elements.Header;
 import com.ss.ita.rozetka.ui.pages.HomePage;
 import com.ss.ita.rozetka.ui.pages.ProductTypePage;
+import com.ss.ita.rozetka.ui.util.PageUtil;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,26 +15,20 @@ public class SearchHistoryTest extends TestRunner {
         int numberSearchTerm = 1;
         String searchTerm = "DELL";
         String partOfUrl = "search";
-        ProductTypePage productsList = new HomePage()
+        Header headerPage = new HomePage()
                 .open()
-                .getHeader()
-                .doSearch(searchTerm);
-        Assert.assertTrue(productsList
-                        .getHeader()
-                        .isUrlContainsText(partOfUrl),
-                "Search Result didn't open");
-        HomePage homePage = productsList
-                .getHeader()
-                .openHomePage();
-        Assert.assertEquals(homePage
-                        .getHeader()
-                        .getCurrentUrl(),
-                "https://rozetka.com.ua/",
-                "Home Page didn't open");
-        String actualSearchTerm = homePage
-                .getHeader()
+                .getHeader();
+        ProductTypePage productsList = headerPage.doSearch(searchTerm);
+        Assert.assertTrue(PageUtil
+                        .getCurrentUrl()
+                        .contains(partOfUrl),
+                "Search Result should be open");
+        HomePage homePage = headerPage.openHomePage();
+        Assert.assertEquals(PageUtil.getCurrentUrl(), "https://rozetka.com.ua/",
+                "Home Page should be open");
+        String actualSearchTerm = headerPage
                 .setSearchInputInFocus()
                 .getTextFromSearchHistory(numberSearchTerm);
         Assert.assertEquals(actualSearchTerm, searchTerm);
-   }
+    }
 }
