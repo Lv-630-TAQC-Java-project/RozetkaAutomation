d9e65e4cd2ac165aa117410a58464d231974a112
dzhyv <d.zhyvko@gmail.com>
2021-11-16 18:00:41 +0200

    Changed asserts

--- src/test/java/com/ss/ita/rozetka/ui/NavigatingToHomePageTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/NavigatingToHomePageTest.java
@@ -4,12 +4,12 @@ import com.ss.ita.rozetka.ui.pages.HomePage;
 import com.ss.ita.rozetka.ui.pages.ProductCategoryPage;
 import com.ss.ita.rozetka.ui.TestUtils.TestRunner;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory.PRODUCTS_FOR_HOUSE;
 import static com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory.KITCHEN_APPLIANCES_CATEGORY;
 import static com.ss.ita.rozetka.ui.util.PageUtil.getCurrentUrl;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NavigatingToHomePageTest extends TestRunner {
 
@@ -18,12 +18,18 @@ public class NavigatingToHomePageTest extends TestRunner {
         ProductCategoryPage page = new HomePage()
                 .open()
                 .openProductCategoryPage(PRODUCTS_FOR_HOUSE);
-        Assert.assertEquals(getCurrentUrl(), "https://bt.rozetka.com.ua/");
+        assertThat(getCurrentUrl())
+                .as("Products for house category page should be opened")
+                .isEqualTo("https://bt.rozetka.com.ua/");
         page.openProductTypePage(KITCHEN_APPLIANCES_CATEGORY);
-        Assert.assertEquals(getCurrentUrl(), "https://bt.rozetka.com.ua/tehnika-dlya-kuhni/c435974/");
+        assertThat(getCurrentUrl())
+                .as("Kitchen appliances category page should be opened")
+                .isEqualTo("https://bt.rozetka.com.ua/tehnika-dlya-kuhni/c435974/");
         page
                 .getHeader()
                 .openHomePage();
-        Assert.assertEquals(getCurrentUrl(), "https://rozetka.com.ua/");
+        assertThat(getCurrentUrl())
+                .as("Home page should be opened")
+                .isEqualTo("https://rozetka.com.ua/");
     }
 }
