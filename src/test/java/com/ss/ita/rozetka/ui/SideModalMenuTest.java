496a85f3d41a18cdf0c167ead9b6a270c7f27fff
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-16 09:48:40 +0200

    fix tests

--- src/test/java/com/ss/ita/rozetka/ui/SideModalMenuTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/SideModalMenuTest.java
@@ -1,6 +1,7 @@
 package com.ss.ita.rozetka.ui;
 
 import com.ss.ita.rozetka.ui.Modals.SideModalMenu;
+import com.ss.ita.rozetka.ui.elements.Header;
 import com.ss.ita.rozetka.ui.pages.HomePage;
 import com.ss.ita.rozetka.ui.runner.TestRunner;
 import org.testng.Assert;
@@ -11,15 +12,10 @@ public class SideModalMenuTest extends TestRunner {
     @Test
     public void verifyOpenCloseSideModalMenu() {
         HomePage homePage = new HomePage().open();
-        SideModalMenu menu = homePage.getHeader().openSideModalMenu();
-        Assert.assertTrue(homePage
-                        .getHeader()
-                        .isSideModalMenuVisible(),
-                "Side Menu have to be visible but is invisible");
+        Header header = homePage.getHeader();
+        SideModalMenu menu = header.openSideModalMenu();
+        Assert.assertTrue(header.isSideModalMenuVisible(), "Side Menu have to be visible but is invisible");
         menu.closeSideModalMenu();
-        Assert.assertFalse(homePage
-                        .getHeader()
-                        .isSideModalMenuVisible(),
-                "Side Menu have to be invisible but is visible");
+        Assert.assertFalse(header.isSideModalMenuVisible(), "Side Menu have to be invisible but is visible");
     }
 }
