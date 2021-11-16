d833612cc34a89b46fa1f2a3178772448d851e9b
percifalll <genero.frido@gmail.com>
2021-11-16 23:29:43 +0200

    Updated close method

--- src/main/java/com/ss/ita/rozetka/ui/Modals/BasketModal.java
+++ src/main/java/com/ss/ita/rozetka/ui/Modals/BasketModal.java
@@ -3,6 +3,7 @@ package com.ss.ita.rozetka.ui.Modals;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.SelenideElement;
 import com.ss.ita.rozetka.ui.pages.OrderingPage;
+import io.qameta.allure.Step;
 
 import java.util.List;
 
@@ -15,27 +16,32 @@ public class BasketModal {
     // with product name as a second argument
     private static final String PRODUCT_XPATH_PATTERN_FOR_NAME = "//single-modal-window//li[contains(., '%s')]";
 
+    @Step("BasketModal: order products you chose")
     public OrderingPage openOrderingPage() {
         $x("//a[contains(@class, 'cart-receipt__submit')]").click();
         return new OrderingPage();
     }
 
+    @Step("BasketModal: get total products price")
     public int getProductsTotalPrice() {
         String sum = $x("//div[contains(@class,'sum-price')]//span[1]")
                 .getAttribute("textContent");
         return Integer.parseInt(sum);
     }
 
+    @Step("BasketModal: check if basket is empty")
     public boolean isEmpty() {
         return $(".cart-dummy").is(exist);
     }
 
+    @Step("BasketModal: get product names ")
     public List<String> getProductNames() {
         return $$("li.cart-list__item a.cart-product__title")
                 .shouldHave(CollectionCondition.sizeGreaterThan(0))
                 .texts();
     }
 
+    @Step("BasketModal: set count for product with name {productName} to {count}")
     public BasketModal setProductCount(String productName, int count) {
         int totalPrice = getProductsTotalPrice();
 
@@ -61,6 +67,7 @@ public class BasketModal {
         }
     }
 
+    @Step("BasketModal: remove product with name {productName}")
     public BasketModal removeProduct(String productName) {
         int totalPrice = getProductsTotalPrice();
 
@@ -73,7 +80,9 @@ public class BasketModal {
         return this;
     }
 
-    public void close() {
+    @Step("BasketModal: continue buying products")
+    public BasketModal close() {
         $x("//button[contains(@class, 'modal__close')]").click();
+        return this;
     }
 }
