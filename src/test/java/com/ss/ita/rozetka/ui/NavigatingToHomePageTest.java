969316b3b6d2e3cdff8d60e27ef92eb1f90aa62d
dzhyv <d.zhyvko@gmail.com>
2021-11-16 17:37:24 +0200

    Fixes
    added getHeader method
    added Kitchen appliances category enum
    changes to test

--- src/test/java/com/ss/ita/rozetka/ui/NavigatingToHomePageTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/NavigatingToHomePageTest.java
@@ -2,34 +2,28 @@ package com.ss.ita.rozetka.ui;
 
 import com.ss.ita.rozetka.ui.pages.HomePage;
 import com.ss.ita.rozetka.ui.pages.ProductCategoryPage;
-import com.ss.ita.rozetka.ui.runner.TestRunner;
+import com.ss.ita.rozetka.ui.TestUtils.TestRunner;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory.PRODUCTS_FOR_HOUSE;
 import static com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory.KITCHEN_APPLIANCES_CATEGORY;
+import static com.ss.ita.rozetka.ui.util.PageUtil.getCurrentUrl;
 
 public class NavigatingToHomePageTest extends TestRunner {
 
     @Test
     public void verifyReturnToHomePage() {
-        ProductCategoryPage page = new HomePage().open()
+        ProductCategoryPage page = new HomePage()
+                .open()
                 .openProductCategoryPage(PRODUCTS_FOR_HOUSE);
-        String productCategoryUrl = page
+        Assert.assertEquals(getCurrentUrl(), "https://bt.rozetka.com.ua/");
+        page.openProductTypePage(KITCHEN_APPLIANCES_CATEGORY);
+        Assert.assertEquals(getCurrentUrl(), "https://bt.rozetka.com.ua/tehnika-dlya-kuhni/c435974/");
+        page
                 .getHeader()
-                .getCurrentUrl();
-        Assert.assertEquals(productCategoryUrl, "https://bt.rozetka.com.ua/");
-        String productSubcategoryUrl = page
-                .openProductTypePage(KITCHEN_APPLIANCES_CATEGORY)
-                .getHeader()
-                .getCurrentUrl();
-        Assert.assertEquals(productSubcategoryUrl, "https://bt.rozetka.com.ua/tehnika-dlya-kuhni/c435974/");
-        String homePageUrl = page
-                .getHeader()
-                .openHomePage()
-                .getHeader()
-                .getCurrentUrl();
-        Assert.assertEquals(homePageUrl, "https://rozetka.com.ua/");
+                .openHomePage();
+        Assert.assertEquals(getCurrentUrl(), "https://rozetka.com.ua/");
     }
 }
