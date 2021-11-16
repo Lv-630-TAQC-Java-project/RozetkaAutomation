e6c02a6cd63deda8b165bedeca315ef4ed711210
dzhyv <d.zhyvko@gmail.com>
2021-11-16 09:49:51 +0200

    Changes

--- src/test/java/com/ss/ita/rozetka/ui/RecentlyViewedProductsTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/RecentlyViewedProductsTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 
 import static com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory.PRODUCTS_FOR_HOUSE;
 import static com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory.KITCHEN_APPLIANCES_CATEGORY;
+import static com.ss.ita.rozetka.ui.util.PageUtil.*;
 
 public class RecentlyViewedProductsTest extends TestRunner {
 
@@ -18,10 +19,7 @@ public class RecentlyViewedProductsTest extends TestRunner {
         ProductTypePage productPage = new HomePage().open()
                 .openProductCategoryPage(PRODUCTS_FOR_HOUSE)
                 .openProductTypePage(KITCHEN_APPLIANCES_CATEGORY);
-        String productTypePageUrl = productPage
-                .getHeader()
-                .getCurrentUrl();
-        Assert.assertEquals(productTypePageUrl, "https://bt.rozetka.com.ua/tehnika-dlya-kuhni/c435974/");
+        Assert.assertEquals(getCurrentUrl(), "https://bt.rozetka.com.ua/tehnika-dlya-kuhni/c435974/");
         String productName = productPage
                 .openProductPage(1)
                 .getName();
