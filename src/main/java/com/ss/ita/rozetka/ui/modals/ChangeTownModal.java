b2937506b7b67b67b5e0598bf98166f8c3460b8a
vladdmytriv <vladdmutriv@gmail.com>
2021-11-16 23:17:04 +0200

    add test with methods

---
+++ src/main/java/com/ss/ita/rozetka/ui/modals/ChangeTownModal.java
@@ -0,0 +1,37 @@
+package com.ss.ita.rozetka.ui.modals;
+
+import com.codeborne.selenide.SelenideElement;
+import com.ss.ita.rozetka.ui.pages.HomePage;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
+
+public class ChangeTownModal {
+
+    @Step("ChangeTownModal: choose town")
+    public ChangeTownModal chooseTown(String town) {
+        $x(format("//a[contains(text(),'%s')]", town)).click();
+        return this;
+    }
+
+    @Step("ChangeTownModal: set town")
+    public ChangeTownModal setTown(String town) {
+        SelenideElement selenideElement = $x("//input[contains(@class, 'autocomplete')]");
+        selenideElement.sendKeys(town);
+        selenideElement.pressEnter();
+        return this;
+    }
+
+    @Step("ChangeTownModal: approve changing town and open home page")
+    public HomePage approveChangingTownAndOpenHomePage(){
+        $x("//a[contains(@class, 'button_color_gray')]").click();
+        return new HomePage() ;
+    }
+
+    @Step("ChangeTownModal:approve changing town")
+    public <PageObject> PageObject approveChangingTown(Class<PageObject> pageObjectClass){
+        $x("//button[contains(@class, 'button_size_medium button_color_green')]").click();
+        return page(pageObjectClass);
+    }
+}
--- src/main/java/com/ss/ita/rozetka/ui/modals/HamburgerModal.java
+++ src/main/java/com/ss/ita/rozetka/ui/modals/HamburgerModal.java
@@ -4,6 +4,7 @@ import com.ss.ita.rozetka.ui.elements.Header;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$x;
+
 // class should be renamed by another PR to SideMenuModal
 public class HamburgerModal {
 
@@ -11,5 +12,16 @@ public class HamburgerModal {
     public Header closeHamburgerModal() {
         $x("//button[@class='side-menu__close']").click();
         return new Header();
+    }
+
+    @Step("SideModalMenu: open change town modal")
+    public ChangeTownModal openChangeTownModal() {
+        $x("//button[contains(@class, 'city-toggle')]").click();
+        return new ChangeTownModal();
+    }
+
+    @Step("SideModalMenu: get town")
+    public String getTown(){
+        return $x("//span[contains(@class, 'city-toggle')]").text();
     }
 }
