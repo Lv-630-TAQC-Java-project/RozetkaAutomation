ddf35911d932df19ee7c04a964ee5c2d34e139c7
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-17 23:15:59 +0200

    add test

--- src/test/java/com/ss/ita/rozetka/ui/SortProductsListTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/SortProductsListTest.java
@@ -4,22 +4,43 @@ import com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory;
 import com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory;
 import com.ss.ita.rozetka.ui.pages.HomePage;
 import com.ss.ita.rozetka.ui.pages.ProductTypePage;
-import com.ss.ita.rozetka.ui.util.SortType;
+import com.ss.ita.rozetka.ui.util.ProductsListSortType;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.sleep;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class SortingProductsListTest {
+import static com.codeborne.selenide.Selenide.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SortProductsListTest {
 
     @Test
-    public void verifySortingFromCheapToExpensiveTest(){
+    public void verifyFromCheapToExpensiveSortingTest(){
         ProductTypePage productsList = new HomePage()
                 .open()
                 .openProductCategoryPage(GeneralProductCategory.COTTAGE_GARDEN_BACKYARD)
                 .openProductTypePage(ProductCategoryAndSubCategory.TRIMMERS_SUBCATEGORY)
-                .sortProductsListBy(SortType.FROM_CHEAP_TO_EXPENSIVE);
-        sleep(10000);
-        productsList.sortProductsListBy(SortType.FROM_EXPENSIVE_TO_CHEAP);
-        sleep(20000);
+                .sortProductsListBy(ProductsListSortType.FROM_CHEAP_TO_EXPENSIVE);
+        int numberProductsListPage = 3;
+        List<BigDecimal> actualOrderPrices = productsList
+                .openProductsListPage(numberProductsListPage)
+                .getProductsListPrices();
+        for (BigDecimal price : actualOrderPrices) {
+            System.out.print(price + ", ");
+        }
+        System.out.println("\n" + actualOrderPrices.size());
+        List<BigDecimal> expectedOrderPrices = actualOrderPrices
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+        for (BigDecimal price : expectedOrderPrices) {
+            System.out.print(price + ", ");
+        }
+        System.out.println("\n" + expectedOrderPrices.size());
+//        assertThat(actualOrderPrices)
+//                .as("Products list should be sorted")
+//                .isEqualTo(expectedOrderPrices);
     }
 }
