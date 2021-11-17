cf06464471a6996302b17a223ab1600f82597145
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-17 08:43:46 +0200

    add test

--- src/test/java/com/ss/ita/rozetka/ui/SortingProductsListTest.java
+++ src/test/java/com/ss/ita/rozetka/ui/SortingProductsListTest.java
@@ -1,2 +1,21 @@
-package com.ss.ita.rozetka.ui;public class SortingProductsListTest {
+package com.ss.ita.rozetka.ui;
+
+import com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory;
+import com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory;
+import com.ss.ita.rozetka.ui.pages.HomePage;
+import com.ss.ita.rozetka.ui.pages.ProductTypePage;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class SortingProductsListTest {
+
+    @Test
+    public void verifySortingFromCheapToExpensiveTest(){
+        ProductTypePage productsList = new HomePage()
+                .open()
+                .openProductCategoryPage(GeneralProductCategory.COTTAGE_GARDEN_BACKYARD)
+                .openProductTypePage(ProductCategoryAndSubCategory.TRIMMERS_SUBCATEGORY);
+        sleep(10000);
+    }
 }
