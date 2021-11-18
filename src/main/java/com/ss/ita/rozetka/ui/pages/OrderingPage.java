9127ca705eb8f0b6a433c7e51dbb41e7b290d1e4
vladdmytriv <vladdmutriv@gmail.com>
2021-11-18 10:38:44 +0200

    "

--- src/main/java/com/ss/ita/rozetka/ui/pages/OrderingPage.java
+++ src/main/java/com/ss/ita/rozetka/ui/pages/OrderingPage.java
@@ -5,13 +5,15 @@ import io.qameta.allure.Step;
 import org.apache.commons.lang3.StringUtils;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.Integer.parseInt;
 
 public class OrderingPage {
 
     @Step("OrderingPage: get total products price")
     public int getTotalProductsPrice() {
-        String price = ($x("//dd[@class = 'checkout-total__value checkout-total__value_size_large']").text());
-        return Integer.parseInt(price.replaceAll("[^0-9]", StringUtils.EMPTY));
+        return parseInt($x("//dd[@class = 'checkout-total__value checkout-total__value_size_large']")
+                .text()
+                .replaceAll("[^0-9]", StringUtils.EMPTY));
     }
 
     @Step("OrderingPage: get sidebar status, assert in test")
