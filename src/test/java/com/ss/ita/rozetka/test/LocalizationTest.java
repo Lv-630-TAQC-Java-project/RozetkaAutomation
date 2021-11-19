ed0893ae48d7e3c45e20435b6a6e3f58f87aea12
NazarYakh <nazaryakh@gmail.com>
2021-11-19 09:48:41 +0200

    fixes

--- src/test/java/com/ss/ita/rozetka/test/LocalizationTest.java
+++ src/test/java/com/ss/ita/rozetka/test/LocalizationTest.java
@@ -1,8 +1,8 @@
 package com.ss.ita.rozetka.test;
 
-import com.ss.ita.rozetka.pageobject.utils.TestRunner;
 import com.ss.ita.rozetka.pageobject.elements.Header;
 import com.ss.ita.rozetka.pageobject.pages.HomePage;
+import com.ss.ita.rozetka.pageobject.utils.TestRunner;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
