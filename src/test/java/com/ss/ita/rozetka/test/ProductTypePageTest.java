e7a9162af8ce8b60d0c33c9ad3da1d44d2439269
anastasia8755 <anastasia.sydor331@gmail.com>
2021-11-19 09:35:46 +0200

    fix

--- src/test/java/com/ss/ita/rozetka/test/ProductTypePageTest.java
+++ src/test/java/com/ss/ita/rozetka/test/ProductTypePageTest.java
@@ -1,12 +1,12 @@
-package com.ss.ita.rozetka;
+package com.ss.ita.rozetka.test;
 
-import com.ss.ita.rozetka.ui.pages.HomePage;
-import com.ss.ita.rozetka.ui.pages.ProductTypePage;
+import com.ss.ita.rozetka.pageobject.pages.HomePage;
+import com.ss.ita.rozetka.pageobject.pages.ProductTypePage;
 import org.testng.annotations.Test;
-import com.ss.ita.rozetka.ui.TestUtils.TestRunner;
+import com.ss.ita.rozetka.pageobject.utils.TestRunner;
 
-import static com.ss.ita.rozetka.ui.ProductsEnum.GeneralProductCategory.NOTEBOOKS_AND_COMPUTERS;
-import static com.ss.ita.rozetka.ui.ProductsEnum.ProductCategoryAndSubCategory.NOTEBOOKS;
+import static com.ss.ita.rozetka.pageobject.product.GeneralProductCategory.NOTEBOOKS_AND_COMPUTERS;
+import static com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory.NOTEBOOKS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductTypePageTest extends TestRunner {
