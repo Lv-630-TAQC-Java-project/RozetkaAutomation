a51d87ca1372f9a95036b9b6e6c213272d5e971d
NazarYakh <nazaryakh@gmail.com>
2021-11-22 16:26:21 +0200

    renamed test class and done some fixes

--- src/test/java/com/ss/ita/rozetka/test/FilterFunctionalityTest.java
+++ src/test/java/com/ss/ita/rozetka/test/FilterFunctionalityTest.java
@@ -1,32 +1,30 @@
 package com.ss.ita.rozetka.test;
 
 import com.ss.ita.rozetka.pageobject.pages.HomePage;
-import com.ss.ita.rozetka.pageobject.pages.ProductPage;
 import com.ss.ita.rozetka.pageobject.pages.ProductTypePage;
 import com.ss.ita.rozetka.pageobject.utils.TestRunner;
 import org.testng.annotations.Test;
 import static com.ss.ita.rozetka.pageobject.utils.PageUtil.*;
 
-
 import static com.ss.ita.rozetka.pageobject.product.GeneralProductCategory.NOTEBOOKS_AND_COMPUTERS;
-import static com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory.MONITORS;
+import static com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory.MONITORS_CATEGORY;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FilterByParametersTest extends TestRunner {
+public class FilterFunctionalityTest extends TestRunner {
 
     @Test
     public void verifyUserCanFilterProducts() {
-        String company = "Samsung";
-        String resolution = "1920x1080";
-
         ProductTypePage productTypePage = new HomePage()
                 .open()
                 .openProductCategoryPage(NOTEBOOKS_AND_COMPUTERS)
-                .openProductTypePage(MONITORS);
+                .openProductTypePage(MONITORS_CATEGORY);
 
         assertThat(getCurrentUrl())
                 .as("Url should contain 'Monitors'")
                 .contains("monitors");
+
+        String company = "Samsung";
+        String resolution = "1920x1080";
 
         productTypePage
                 .filterProductsByParameters(company)
@@ -36,9 +34,9 @@ public class FilterByParametersTest extends TestRunner {
                 .as("Title should be Samsung")
                 .contains(company);
 
-        ProductPage productPage = new ProductTypePage().openProductPage(1);
-
-        assertThat(productPage.getProductCharacteristics())
+        assertThat(productTypePage
+                .openProductPage(1)
+                .getProductCharacteristics())
                 .as("In characteristics should be 1920 x 1080 resolution")
                 .contains("1920 x 1080");
     }
