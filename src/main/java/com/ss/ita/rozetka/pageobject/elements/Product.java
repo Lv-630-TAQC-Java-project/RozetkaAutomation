65673f7f742068fe8259988e0503f2ad99b5111b
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-24 11:31:48 +0200

    class Product

--- src/main/java/com/ss/ita/rozetka/pageobject/elements/Product.java
+++ src/main/java/com/ss/ita/rozetka/pageobject/elements/Product.java
@@ -1,45 +1,45 @@
 package com.ss.ita.rozetka.pageobject.elements;
 
-import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.support.Color;
+
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Selenide.*;
 
+@RequiredArgsConstructor
 public class Product {
-    @Getter
+
+    @NonNull
+    private final String xPath;
     private String promoLabelTitle;
-    @Getter
     private String productTitle;
-    @Getter
     private BigDecimal oldProductPrice;
-    @Getter
     private BigDecimal productPrice;
-    @Getter
     private List<String> availableColors;
-    @Getter
     private String availability;
-    @Getter
     private int amountReviews;
-    @Getter
     private String productDescription;
 
-    private void setProductTitle(String xPath) {
+    public String getProductTitle() {
         productTitle = $x(String.format("%s%s", xPath, "//span[@class='goods-tile__title']")).text();
+        return productTitle;
     }
 
-    private void setPromoLabelTitle(String xPath) {
+    public String getPromoLabelTitle() {
         try {
             promoLabelTitle = $x(String.format("%s%s", xPath, "//span[contains(@class,'goods-tile__label')]")).text();
         } catch (AssertionError exception) {
             promoLabelTitle = StringUtils.EMPTY;
         }
+        return promoLabelTitle;
     }
 
-    private void setAvailableColors(String xPath) {
+    public List<String> getAvailableColors() {
         try {
             availableColors = $$x(String.format("%s%s", xPath, "//span[@class='goods-tile__colors-content']"))
                     .stream()
@@ -48,9 +48,10 @@ public class Product {
         } catch (AssertionError exception) {
             availableColors = null;
         }
+        return availableColors;
     }
 
-    private void setOldProductPrice(String xPath) {
+    public BigDecimal getOldProductPrice() {
         try {
             String oldPriceString = $x(String.format("%s%s", xPath, "//div[contains(@class,'goods-tile__price--old')]"))
                     .text()
@@ -59,9 +60,10 @@ public class Product {
         } catch (AssertionError exception) {
             oldProductPrice = BigDecimal.ZERO;
         }
+        return oldProductPrice;
     }
 
-    private void setProductPrice(String xPath){
+    public BigDecimal getProductPrice() {
         try {
             String price = $x(String.format("%s%s", xPath, "//span[contains(@class,'goods-tile__price-value')]"))
                     .text()
@@ -70,17 +72,19 @@ public class Product {
         } catch (AssertionError exception) {
             productPrice = BigDecimal.ZERO;
         }
+        return productPrice;
     }
 
-    private void setAvailability(String xPath) {
+    public String getAvailability() {
         try {
-            this.availability = $x(String.format("%s%s", xPath, "//div[contains(@class,'goods-tile__availability')]")).text();
+            availability = $x(String.format("%s%s", xPath, "//div[contains(@class,'goods-tile__availability')]")).text();
         } catch (AssertionError exception) {
-            this.availability = StringUtils.EMPTY;
+            availability = StringUtils.EMPTY;
         }
+        return availability;
     }
 
-    private void setAmountReviews(String xPath) {
+    public int getAmountReviews() {
         try {
             amountReviews = Integer.parseInt($x(String.format("%s%s", xPath, "//span[contains(@class,'goods-tile__reviews-link')]"))
                     .text()
@@ -88,25 +92,16 @@ public class Product {
         } catch (AssertionError exception) {
             amountReviews = 0;
         }
+        return amountReviews;
     }
 
-    private void setProductDescription(String xPath){
-        try{
+    public String getProductDescription() {
+        try {
             actions().moveToElement($x(xPath)).perform();
             productDescription = $x(String.format("%s%s", xPath, "//*[contains(@class,'goods-tile__description')]")).text();
-        }catch(AssertionError exception){
+        } catch (AssertionError exception) {
             productDescription = StringUtils.EMPTY;
         }
+        return productDescription;
     }
-
-    public Product(String xPath) {
-        setProductTitle(xPath);
-        setPromoLabelTitle(xPath);
-        setOldProductPrice(xPath);
-        setProductPrice(xPath);
-        setAmountReviews(xPath);
-        setAvailability(xPath);
-        setAvailableColors(xPath);
-        setProductDescription(xPath);
-   }
 }
