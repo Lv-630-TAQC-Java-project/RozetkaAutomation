bb9cce020a58a1656be6ae4b45dfe46816ee76cd
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-24 13:18:32 +0200

    add test method

---
+++ src/test/java/com/ss/ita/rozetka/test/SortProductsListTest.java
@@ -0,0 +1,12 @@
+package com.ss.ita.rozetka.test;
+
+import com.ss.ita.rozetka.pageobject.utils.TestRunner;
+import org.testng.annotations.Test;
+
+public class SortProductsListTest extends TestRunner {
+
+    @Test
+    public void verifyActionSortingFunctionality(){
+
+    }
+}
