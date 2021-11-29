a7e20335581c333fac93859cf0c38d557f968eeb
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-29 09:08:23 +0200

    modify isOpen method

--- src/main/java/com/ss/ita/rozetka/pageobject/pages/ProductCategoryPage.java
+++ src/main/java/com/ss/ita/rozetka/pageobject/pages/ProductCategoryPage.java
@@ -17,7 +17,13 @@ public class ProductCategoryPage extends HeaderPage {
 
     @Step("ProductCategoryPage: get product category page visibility status by locating page heading")
     public Boolean isOpened() {
-        return $x("//h1[@class = 'portal__heading ng-star-inserted']").is(Condition.visible);
+        try {
+            return $x("//h1[@class = 'portal__heading ng-star-inserted']")
+                    .should(Condition.exist)
+                    .is(Condition.visible);
+        } catch (AssertionError exception) {
+            return false;
+        }
     }
 }
 
