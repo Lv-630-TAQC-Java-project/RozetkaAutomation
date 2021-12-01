ba7d730e36b9f9112f803f4e968cb9495c72b40f
VladGranat <simonphenex@gmail.com>
2021-12-01 21:47:05 +0200

    Try open page of category

---
+++ src/test/java/com/ss/ita/rozetka/test/ProductTypePageTest2.java
@@ -0,0 +1,19 @@
+package com.ss.ita.rozetka.test;
+
+import com.ss.ita.rozetka.pageobject.pages.HomePage;
+import com.ss.ita.rozetka.pageobject.pages.ProductTypePage;
+import com.ss.ita.rozetka.pageobject.utils.TestRunner;
+import org.testng.annotations.Test;
+
+import static com.ss.ita.rozetka.pageobject.product.GeneralProductCategory.STATIONERY_AND_BOOKS;
+import static com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory.BOOKS_CATEGORY;
+
+public class ProductTypePageTest2 extends TestRunner {
+    @Test
+    public void verifyThatProductsArePresentedInBooksCategory(){
+        ProductTypePage productTypePage = new HomePage()
+                .open()
+                .openProductCategoryPage(STATIONERY_AND_BOOKS)
+                .openProductTypePage(BOOKS_CATEGORY);
+    }
+}
