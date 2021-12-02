9add6db59d8c55b5f866d966996edda4bbd278cc
anastasia8755 <anastasia.sydor331@gmail.com>
2021-12-02 02:14:24 +0200

    fix

--- src/test/java/com/ss/ita/rozetka/test/ProductTypePageTest.java
+++ src/test/java/com/ss/ita/rozetka/test/ProductTypePageTest.java
@@ -2,8 +2,6 @@ package com.ss.ita.rozetka.test;
 
 import com.ss.ita.rozetka.pageobject.pages.HomePage;
 import com.ss.ita.rozetka.pageobject.pages.ProductTypePage;
-import com.ss.ita.rozetka.pageobject.product.GeneralProductCategory;
-import com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory;
 import com.ss.ita.rozetka.pageobject.utils.TestRunner;
 import org.testng.annotations.Test;
 
@@ -13,25 +11,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductTypePageTest extends TestRunner {
 
-    private ProductTypePage openSelectedCategory
-            (GeneralProductCategory generalProductCategory, ProductCategoryAndSubCategory productCategoryAndSubCategory) {
-        ProductTypePage productTypePage = new HomePage()
-                .open()
-                .openProductCategoryPage(generalProductCategory)
-                .openProductTypePage(productCategoryAndSubCategory);
-        return productTypePage;
-    }
-
     @Test
     public void verifyThatProductsArePresentedInNotebookCategory() {
-        assertThat(openSelectedCategory(NOTEBOOKS_AND_COMPUTERS, NOTEBOOKS_CATEGORY).getProductsCount())
+        ProductTypePage productTypePage = new HomePage()
+                .open()
+                .openProductCategoryPage(NOTEBOOKS_AND_COMPUTERS)
+                .openProductTypePage(NOTEBOOKS_CATEGORY);
+
+        assertThat(productTypePage.getProductsCount())
                 .as("There should be presented at least 10 products")
                 .isGreaterThan(10);
     }
 
     @Test
     public void verifyThatProductsArePresentedInTVAccessoriesCategory() {
-        assertThat(openSelectedCategory(PHONES_AND_TV, TV_ACCESSORIES_CATEGORY).getProductsCount())
+        ProductTypePage productTypePage = new HomePage()
+                .open()
+                .openProductCategoryPage(PHONES_AND_TV)
+                .openProductTypePage(TV_ACCESSORIES_CATEGORY);
+
+        assertThat(productTypePage.getProductsCount())
                 .as("There should be presented at least 10 products")
                 .isGreaterThan(10);
     }
