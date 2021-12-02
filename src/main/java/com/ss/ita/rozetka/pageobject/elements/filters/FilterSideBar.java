a63138c8d08b5cba3a4e363d2b21fac8dcca50c2
percifalll <genero.frido@gmail.com>
2021-12-02 23:21:07 +0200

    Removed unnecessary comments. Refactoring. Removed wrong formatting

--- src/main/java/com/ss/ita/rozetka/pageobject/elements/filters/FilterSideBar.java
+++ src/main/java/com/ss/ita/rozetka/pageobject/elements/filters/FilterSideBar.java
@@ -15,11 +15,8 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
-/**
- * Represents a general filter stack on product type page.
- * Can create Filter instances and work with price filter
- */
 public class FilterSideBar {
     private final String minPriceFieldXpath = "//rz-filter-stack//input[@formcontrolname='min']";
     private final String maxPriceFieldXpath = "//rz-filter-stack//input[@formcontrolname='max']";
@@ -85,10 +82,6 @@ public class FilterSideBar {
         return this;
     }
 
-    /**
-     * Represents a single filter block with options and, if exists, a search field.<br>
-     * <b>Can be created only from FilterSideBar.</b>
-     */
     @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Filter {
         private final String filterContainerXpath;
@@ -127,7 +120,7 @@ public class FilterSideBar {
         public String getTitle() {
             return $x(filterContainerXpath + "//span[@class='sidebar-block__toggle-title']")
                     .text()
-                    .replaceAll("\\d", "")
+                    .replaceAll("\\d", EMPTY)
                     .trim();
         }
 
@@ -136,7 +129,7 @@ public class FilterSideBar {
             return $$x(filterContainerXpath + "//input[@class='custom-checkbox']/following-sibling::label")
                     .texts()
                     .stream()
-                    .map(name -> name.replaceAll("[\\(]\\d+[\\)]", ""))
+                    .map(name -> name.replaceAll("[\\(]\\d+[\\)]", EMPTY))
                     .map(String::trim)
                     .collect(Collectors.toList());
         }
@@ -146,9 +139,7 @@ public class FilterSideBar {
             SelenideElement optionCheckBox = getOptionCheckBox(optionName);
 
             optionCheckBox.shouldNotBe(checked);
-            getOptionCheckBoxLink(optionName)
-                    .scrollIntoView(false)
-                    .click();
+            getOptionCheckBoxLink(optionName).scrollIntoView(false).click();
 
             optionCheckBox.shouldBe(checked);
             return this;
@@ -159,9 +150,7 @@ public class FilterSideBar {
             SelenideElement optionCheckBox = getOptionCheckBox(optionName);
 
             optionCheckBox.shouldBe(checked);
-            getOptionCheckBoxLink(optionName)
-                    .scrollIntoView(false)
-                    .click();
+            getOptionCheckBoxLink(optionName).scrollIntoView(false).click();
 
             optionCheckBox.shouldNotBe(checked);
             return this;
@@ -172,10 +161,6 @@ public class FilterSideBar {
             return getOptionCheckBox(optionName).isSelected();
         }
 
-        /**
-         * @return quantity of options that filter has
-         * Example: "Seller (3)"
-         */
         @Step("Filter: get quantity of options in filter")
         public int getOptionsQuantityInFilter() {
             return Integer.parseInt(
@@ -183,39 +168,34 @@ public class FilterSideBar {
             );
         }
 
-        /**
-         * @return quantity of products that corresponds to filter option
-         * Example: "Asus (28)"
-         */
         @Step("Filter: get products quantity that corresponds to option with name {optionName}")
         public int getProductsQuantityOfOption(String optionName) {
-            String optionQuantityXpath =
-                    format(filterContainerXpath + "//label[@for='%s']/span", optionName);
+            String optionQuantityXpath = format(filterContainerXpath + "//label[@for='%s']/span", optionName);
 
             return Integer.parseInt(
                     $x(optionQuantityXpath)
                             .text()
-                            .replaceAll("\\D|\\s", "")
+                            .replaceAll("\\D|\\s", EMPTY)
             );
         }
 
         @Step("Filter: get search field presence status")
-        public boolean hasOptionsSearch() {
+        public boolean hasOptionsFilter() {
             return getSearchField().is(exist);
         }
 
-        @Step("Filter: set search term {searchTerm} to filter options")
-        public Filter searchOptions(String searchTerm) {
+        @Step("Filter: filter options by term {filterTerm}")
+        public Filter filterOptions(String filterTerm) {
             int optionsQuantityBeforeSearch = getAllOptionCheckBoxes().size();
 
-            getSearchField().sendKeys(searchTerm);
+            getSearchField().sendKeys(filterTerm);
 
             getAllOptionCheckBoxes().shouldHave(sizeLessThan(optionsQuantityBeforeSearch));
             return this;
         }
 
         @Step("Filter: clear search field")
-        public Filter clearSearch() {
+        public Filter clearOptionsFilter() {
             int optionsQuantityBeforeClearing = getAllOptionCheckBoxes().size();
 
             $x(filterContainerXpath + "//button[contains(@class,'sidebar-search__clear')]").click();
@@ -225,7 +205,7 @@ public class FilterSideBar {
         }
 
         @Step("Filter: get all selected options")
-        public List<String> getNamesOfSelectedOptions() {
+        public List<String> getSelectedOptionsNames() {
             return getAllOptionCheckBoxes()
                     .filter(checked)
                     .stream()
