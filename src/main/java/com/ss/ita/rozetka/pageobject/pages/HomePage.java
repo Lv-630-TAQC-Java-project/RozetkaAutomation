680a46e190973560639c5c6720b2043245414c80
dzhyv <d.zhyvko@gmail.com>
2021-12-06 09:18:08 +0200

    fix

--- src/main/java/com/ss/ita/rozetka/pageobject/pages/HomePage.java
+++ src/main/java/com/ss/ita/rozetka/pageobject/pages/HomePage.java
@@ -43,7 +43,9 @@ public class HomePage extends HeaderPage {
 
     @Step("HomePage: get recently viewed products names list from first to {intNumber} Recently Viewed Products list")
     public List<String> getRecentlyViewedProductNames() {
-        return $$x("//section[@class = 'main-goods ng-star-inserted'][1]//ul/li//a[@class = 'tile__title']").shouldBe(sizeNotEqual(0)).texts();
+        return $$x("//section[@class = 'main-goods ng-star-inserted'][1]//ul/li//a[@class = 'tile__title']")
+                .shouldBe(sizeNotEqual(0))
+                .texts();
     }
 
     @Step("HomePage: get display status main menu categories")
