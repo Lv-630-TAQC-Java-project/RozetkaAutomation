7d3a0b12046e4d0779dd8695ded1af3276612a88
anastasia8755 <anastasia.sydor331@gmail.com>
2021-12-06 04:13:05 +0200

    add annotations

--- src/test/java/com/ss/ita/rozetka/test/CatalogModalTest.java
+++ src/test/java/com/ss/ita/rozetka/test/CatalogModalTest.java
@@ -3,12 +3,16 @@ package com.ss.ita.rozetka.test;
 import com.ss.ita.rozetka.pageobject.elements.Header;
 import com.ss.ita.rozetka.pageobject.pages.HomePage;
 import com.ss.ita.rozetka.pageobject.utils.TestRunner;
+import io.qameta.allure.Description;
+import io.qameta.allure.TmsLink;
 import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
 public class CatalogModalTest extends TestRunner {
 
     @Test
+    @Description("Verify that catalog opens and closed correctly")
+    @TmsLink(value = "LVTAQC630-13")
     public void verifyThatCatalogOpensAndCloses() {
         Header header = new HomePage()
                 .open()
