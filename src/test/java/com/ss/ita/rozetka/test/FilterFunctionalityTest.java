82f3a1275b00f978a152473dc1b3f0d085f046f0
anastasia8755 <anastasia.sydor331@gmail.com>
2021-12-08 14:19:54 +0200

    start test

--- src/test/java/com/ss/ita/rozetka/test/FilterFunctionalityTest.java
+++ src/test/java/com/ss/ita/rozetka/test/FilterFunctionalityTest.java
@@ -1,5 +1,6 @@
 package com.ss.ita.rozetka.test;
 
+import com.ss.ita.rozetka.pageobject.elements.filters.FilterSideBar;
 import com.ss.ita.rozetka.pageobject.modals.BasketModal;
 import com.ss.ita.rozetka.pageobject.pages.HomePage;
 import com.ss.ita.rozetka.pageobject.pages.ProductPage;
@@ -11,6 +12,7 @@ import org.testng.annotations.Test;
 
 import static com.ss.ita.rozetka.pageobject.product.GeneralProductCategory.NOTEBOOKS_AND_COMPUTERS;
 import static com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory.MONITORS_CATEGORY;
+import static com.ss.ita.rozetka.pageobject.product.ProductCategoryAndSubCategory.NOTEBOOKS_CATEGORY;
 import static com.ss.ita.rozetka.pageobject.utils.PageUtil.getCurrentUrl;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -58,5 +60,39 @@ public class FilterFunctionalityTest extends TestRunner {
         assertThat(totalPriceWithoutOptions)
                 .as("Total price should be changed")
                 .isLessThan(totalPriceWithOptions);
+    }
+
+    @Test
+    @Description("Verify that after changing and submitting price bounds displayed products with correct price")
+    @TmsLink(value = "LVTAQC630-55")
+    public void verifyThatPriceFilterIsCorrect() {
+        var productTypePage = new HomePage()
+                .open()
+                .openProductCategoryPage(NOTEBOOKS_AND_COMPUTERS)
+                .openProductTypePage(NOTEBOOKS_CATEGORY);
+
+        var company ="Dell";
+
+        productTypePage
+                .filterProductsByParameters(company);
+
+        assertThat(productTypePage.getProductTitle(1))
+                .as("Title should contains Dell")
+                .contains(company);
+
+        FilterSideBar filterSideBar = new FilterSideBar()
+                .setMinPrice(15000)
+                .setMaxPrice(50000);
+
+        productTypePage.getProductPricesList().stream().filter(s->s>=15000&&s<=50000);
+        int productPrisesCount=productTypePage.getProductPricesList().size();
+        productTypePage.getProductPricesList().size();
+
+        boolean status = false;
+        for (int i = 0; i <= productPrisesCount; i++) {
+            if (productTypePage.getProductPricesList().get(i) > 50000 && productTypePage.getProductPricesList().get(i) < 15000) {
+                status = true;
+            } else status = false;
+        }
     }
 }
