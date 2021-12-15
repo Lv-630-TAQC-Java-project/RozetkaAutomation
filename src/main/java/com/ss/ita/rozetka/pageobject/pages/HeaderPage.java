a7e8c7bc923504301063b5dfaab7e4f2b819addf
dzhyv <d.zhyvko@gmail.com>
2021-12-15 09:10:46 +0200

    Added step

--- src/main/java/com/ss/ita/rozetka/pageobject/pages/HeaderPage.java
+++ src/main/java/com/ss/ita/rozetka/pageobject/pages/HeaderPage.java
@@ -2,13 +2,15 @@ package com.ss.ita.rozetka.pageobject.pages;
 
 import com.codeborne.selenide.Selenide;
 import com.ss.ita.rozetka.pageobject.elements.Header;
+import io.qameta.allure.Step;
 import lombok.Getter;
 
 public abstract class HeaderPage {
     @Getter
     protected Header header = new Header();
 
-    public Header back(){
+    @Step("Return to previous page")
+    public Header back() {
         Selenide.back();
         return new Header();
     }
