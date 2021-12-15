d3761c6d682623afc92bf2f68fb8120ea8d7e8af
NazarYakh <nazaryakh@gmail.com>
2021-12-15 12:10:59 +0200

    changed error message

--- src/test/java/com/ss/ita/rozetka/test/AddProductToWishListTest.java
+++ src/test/java/com/ss/ita/rozetka/test/AddProductToWishListTest.java
@@ -58,7 +58,7 @@ public class AddProductToWishListTest extends TestRunner {
                 .removeProduct();
 
         assertThat(wishListPage.isProductListEmpty())
-                .as("Product title should not be the same")
+                .as("Product list size should be equal to zero")
                 .isTrue();
     }
 }
